e45e6c6359e3fd92b905ecb611b6df199788b4a6
Gbemumu <sethprogram@gmail.com>
2023-06-19 16:20:27 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
+#include <stdio.h>
+/*
+ * main - entry point
+ *
+ * returns - always returns 0(success)
+*/
 
-
+int main(void)
+{
+	puts(/"Programming is like building a multilingual puzzle");
+	return(0);
+}
