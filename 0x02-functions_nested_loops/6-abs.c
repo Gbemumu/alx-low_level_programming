0b7f86be73ce4769b5d118a11edf7cc02823a377
gbemumu <sethprogram@gmail.com>
2023-09-14 09:25:30 -0700

    Printing absolute value of an int

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * int _abs - prints the absolute of an int
+ *
+ * @n: int to be checked
+ *
+ * Return: -n if n is negative and n if n is positive
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else 
+		return (n);
+}
