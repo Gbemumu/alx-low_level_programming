5dd79090cfa599e47143708411c503b5e68de689
gbemumu <sethprogram@gmail.com>
2023-09-15 01:29:07 -0700

    Checks for digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isupper - check char for an uppercase
+ *
+ * @c: char to check
+ *
+ * Return: 1 if c is uppercase
+ *         0 otherwise
+ */
+int _isdigit(int c)
+
+{
+        if (c >= '0' && c <= '9')
+        {
+                return (1);
+        }
+
+        else
+        {
+                return (0);
+        }
+}
