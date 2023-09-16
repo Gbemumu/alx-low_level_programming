ba95547a25bbd0ed2bcae1a79c790fb1a157e478
gbemumu <sethprogram@gmail.com>
2023-09-16 10:32:00 -0700

    Print more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints more numbers
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+
+	char a;
+
+	for (a = '0'; a <= '14'; a++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+	
+}
