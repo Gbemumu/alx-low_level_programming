2e870ae0a002795d3ffd06033603826c42b72272
gbemumu <sethprogram@gmail.com>
2023-09-18 09:05:18 -0700

    Print ;ines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,21 +10,18 @@
 
 void print_line(int n)
 {
-
 	if (n <= 0)
 	{
 		_putchar('\n');
-	}
-
-	else
+	} else
 	{
 		int i;
 
 		for (i = 1; i <= n; i++)
 		{
-			_putchar('-');
+			_putchar('_');
 		}
-	
 		_putchar('\n');
 	}
-}
+
+}              
